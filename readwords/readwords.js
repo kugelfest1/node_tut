06ee24e2f70d581a6b9ea508a0239e318da467b4
ubi <ubi@elitebook.localdomain>
2026-03-11 11:58:05 +0100

    2026 nodejs refresher tut

--- readwords/readwords.js
+++
@@ -1,6 +0,0 @@
-var censor = require("censorify32767");
-console.log(censor.getCensorWords());
-console.log(censor.censor("some very sad, bad and mad text"));
-censor.addCensorWord("gloomy");
-console.log(censor.getCensorWords());
-console.log(censor.censor("A very gloomy day"));
